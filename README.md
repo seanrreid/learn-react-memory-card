3c15dc69c0034d7cc6c640dcd4b20dae8217aa1d
Sean R Reid <sean@torchcodelab.com>
2020-03-25 07:53:41 -0400

    Added steps from part 2 * Card has an inner div, and a front and back * State tracks whether or not a card should be flipped * Classes change based on the state for isFlipped

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 # React Memory Card Game: Digital Crafts Exercises
 
 - Part 1 instructions: https://docs.google.com/document/d/1gLjQFQDiN_VqTxmb4RL83jzJOHzcLDlqnThYlmaRqTg/edit#
-- Part 2 instructions:
+- Part 2 instructions: https://docs.google.com/document/d/118f456XBWMy4WaqTcvTo10QEPSsI_prz12q7DDHT4KM/edit?usp=sharing
 - Part 3 Instructions:
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+  state = {
+    isFlipped: false
+  };
+
+  clickHandler = () => {
+    this.setState({
+      isFlipped: !this.state.isFlipped
+    });
+  };
+
   render() {
+    const { isFlipped } = this.state;
+
+    const memoryCardInnerClass =
+      'MemoryCardInner ' + (isFlipped === true && 'flipped');
+
     return (
-      <div className="MemoryCard">
-        <img
-          src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"
-          alt="Digital Crafts Logo"
-        />
+      <div className="MemoryCard" onClick={this.clickHandler}>
+        <div className={memoryCardInnerClass}>
+          <div className="MemoryCardBack">
+            <img
+              src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"
+              alt="Digital Crafts Logo"
+            />
+          </div>
+          <div className="MemoryCardFront">
+            <p>∆</p>
+          </div>
+        </div>
       </div>
     );
   }
